f1136707a01ca6bfed4edafa21619497d9a87bcf
yukseltron <h.yuksel04@gmail.com>
2017-10-24 00:15:38 -0400

    json parser needs to be made

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function AppViewModel() {
-	var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel + "&limit=1&offset=0&rating=PG-13&lang=en"
+	this.channel = ko.observable("Charlie Brown");
+	var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en"
 	var gif = $.get(query);
-	$("#tv").src = gif.data[url];
+	console.log("EAD:",gif)
+	var json = JSON.parse(gif);
+
+	$("#tv").src = json.responseJSON.data.url;
 }
 
 // Activates knockout.js
