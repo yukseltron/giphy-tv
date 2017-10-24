aa028c7aa7683be8155c2cd4299ec267ed940cdc
yukseltron <h.yuksel04@gmail.com>
2017-10-24 10:44:07 -0400

    json parser problems

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 function AppViewModel() {
-	this.channel = ko.observable("Charlie Brown");
-	var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en"
-	var gif = $.get(query);
-	console.log("EAD:",gif)
-	var json = JSON.parse(gif);
+	this.channel = ko.observable("cheese");
 
-	$("#tv").src = json.responseJSON.data.url;
+	var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en";
+	var request = new XMLHttpRequest();
+	request.open('GET', query);
+
+	console.log("EAD:", request);
+
+	$("#tv").attr('src', request);
+
+
+/*
+	var request = new XMLHttpRequest();
+	request.open('GET', query);
+
+	request.onload = function(){
+    	var data  = JSON.parse(request.responseText);
+    	console.log("EAD:", data[0]);
+	};
+
+
+	this.url = ko.observable(query);
+	*/
 }
 
 // Activates knockout.js
