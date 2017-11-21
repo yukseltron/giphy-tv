8b6e4f8438c4ed6f2d10462ed6402d0f8672c7b6
yukseltron <h.yuksel04@gmail.com>
2017-11-21 13:04:08 -0500

    api call fixed

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 function AppViewModel() {
-	this.channel = ko.observable("tv");
-
-	var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en";
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", query, false);
-	console.log(xhr.status);
-
+	this.channel = ko.observable("cheese");
 	this.url = ko.observable();
+	var img;
+	var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en";
+
+	$.getJSON(query, function (json) {
+		console.log(json.data[0].bitly_gif_url);
+		img = json.data[0].bitly_gif_url;
+			console.log("IMG1:",img);
+	});
+	console.log("IMG2:",img);
+	this.url = img;
 }
 
 // Activates knockout.js
