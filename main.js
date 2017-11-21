2a99044a3bbad1af90c273b0a44a6012051e7ba5
yukseltron <h.yuksel04@gmail.com>
2017-11-20 21:54:18 -0500

    adding logic;

--- main.js
+++ main.js
@@ -1,27 +1,9 @@
 function AppViewModel() {
-	this.channel = ko.observable("cheese");
+	this.channel = ko.observable();
 
-	var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en";
-	var request = new XMLHttpRequest();
-	request.open('GET', query);
+	console.log(this.channel());
+//	var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en";
 
-	console.log("EAD:", request);
-
-	this.url = request;
-
-
-/*
-	var request = new XMLHttpRequest();
-	request.open('GET', query);
-
-	request.onload = function(){
-    	var data  = JSON.parse(request.responseText);
-    	console.log("EAD:", data[0]);
-	};
-
-
-	this.url = ko.observable(query);
-	*/
 }
 
 // Activates knockout.js
