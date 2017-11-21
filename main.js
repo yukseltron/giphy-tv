aa696d66aa9a68bd33ff62ec5d638101d9192dbc
yukseltron <h.yuksel04@gmail.com>
2017-11-21 14:10:22 -0500

    updates

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 function AppViewModel() {
-	this.channel = ko.observable("cheese");
-	this.url = ko.observable();
-	var img;
-	var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en";
-
-	$.getJSON(query, function (json) {
-		console.log(json.data[0].bitly_gif_url);
-		img = json.data[0].bitly_gif_url;
-			console.log("IMG1:",img);
+	$.ajaxSetup({
+    	async: false
 	});
-	console.log("IMG2:",img);
-	this.url = img;
+
+	this.channel = ko.observable("tv");
+	console.log(this.channel());
+
+	this.url = ko.computed(function() {
+		var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en";
+		var img;
+
+		$.getJSON(query, function (json) {
+			img = json.data[0].images.original.url;
+		});
+
+		return img;
+	}, this);
 }
 
 // Activates knockout.js
