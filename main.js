3941e9a9042303d9bc3e32975e7d48ec2283245e
yukseltron <h.yuksel04@gmail.com>
2017-11-20 22:29:21 -0500

    more logic fixes

--- main.js
+++ main.js
@@ -2,16 +2,11 @@ function AppViewModel() {
 	this.channel = ko.observable("tv");
 
 	var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en";
-	var json = $.getJSON(query);
-	var obj = JSON.parse(json);
-	console.log(obj)
-
-	var channelTag = obj.data[3];
-	console.log("Q1:",obj.data[3]);
-	console.log("Q2:",query);
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", query, false);
+	console.log(xhr.status);
 
 	this.url = ko.observable();
-	this.url = channelTag;
 }
 
 // Activates knockout.js
