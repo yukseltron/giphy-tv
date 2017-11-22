0147550f8fc674d6f921755c3ef8413f317229ad
yukseltron <h.yuksel04@gmail.com>
2017-11-22 12:52:38 -0500

    added instructions

--- main.js
+++ main.js
@@ -17,18 +17,17 @@ function AppViewModel() {
 			img = "https://media.giphy.com/media/lCkumTggV53xe/giphy.gif";//tv static gif for default
 		} else {
 			query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + chan + "&limit=20&offset=0&rating=PG-13&lang=en";
-		}
 
-		$.getJSON(query, function(json) {
-			console.log("length:",json.data.length);
-			if (json.data.length == 0) {
-				img = "https://media.giphy.com/media/lCkumTggV53xe/giphy.gif";
-			} else {
-				rand = Math.floor(Math.random() * json.data.length)
-				img = json.data[rand].images.original.url;
-			}
-		});
-										console.log(rand);
+			$.getJSON(query, function(json) {
+				console.log("length:",json.data.length);
+				if (json.data.length == 0) {
+					img = "https://media2.giphy.com/media/Yqn9tE2E00k4U/giphy.gif";
+				} else {
+					rand = Math.floor(Math.random() * json.data.length)
+					img = json.data[rand].images.original.url;
+				}
+			});
+		}
 
 		return img;
 	}, this);
