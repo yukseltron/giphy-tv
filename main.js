2f7951e524f21913c7bf6b760ab34bf63219ae4b
yukseltron <h.yuksel04@gmail.com>
2017-11-22 11:14:17 -0500

    static  when no channel

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ function AppViewModel() {
     	async: false
 	});
 
-	this.channel = ko.observable("static");
+	this.channel = ko.observable("");
+	var chan;
+	var query;
 
 	this.url = ko.computed(function() {
-		var query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + this.channel() + "&limit=1&offset=0&rating=PG-13&lang=en";
 		var img;
+		chan = this.channel();
+
+		if (chan === "") {
+			query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=static&limit=1&offset=0&rating=PG-13&lang=en";
+		} else {
+			query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + chan + "&limit=1&offset=0&rating=PG-13&lang=en";
+		}
+
 
 		$.getJSON(query, function (json) {
 			img = json.data[0].images.original.url;
