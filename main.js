7385d720cfb55448eff6e83dfe5e798a2d2e5c69
yukseltron <h.yuksel04@gmail.com>
2017-11-22 13:54:38 -0500

    fixed bug for static screen

--- main.js
+++ main.js
@@ -4,26 +4,25 @@ function AppViewModel() {
 	});
 
 	this.channel = ko.observable("");
-	var chan;
-	var query;
 
 	//url for img src
 	this.url = ko.computed(function() {
-		chan = this.channel();
-		var img = "https://media2.giphy.com/media/Yqn9tE2E00k4U/giphy.gif";//tv static gif for default
-		var rand;
+		var query;
+		var chan = this.channel();
+		var img = "https://media.giphy.com/media/iF3M9gPPCdulq/giphy.gif";
+
 
 		if (chan == "") {
-			img = "https://media.giphy.com/media/lCkumTggV53xe/giphy.gif";//tv static gif for default
+			img = "https://media.giphy.com/media/iF3M9gPPCdulq/giphy.gif";//tv static gif for default
 		} else {
 			query = "https://api.giphy.com/v1/gifs/search?api_key=SPI6LMY5gmLf6GFxoQwehrP6mTm6yE0I&q=" + chan + "&limit=20&offset=0&rating=PG-13&lang=en";
 
 			$.getJSON(query, function(json) {
 				console.log("length:",json.data.length);
 				if (json.data.length == 0) {
-					img = "https://media2.giphy.com/media/Yqn9tE2E00k4U/giphy.gif";
+					img = "https://media.giphy.com/media/iF3M9gPPCdulq/giphy.gif";
 				} else {
-					rand = Math.floor(Math.random() * json.data.length)
+					var rand = Math.floor((Math.random() * json.data.length) + 0);
 					img = json.data[rand].images.original.url;
 				}
 			});
